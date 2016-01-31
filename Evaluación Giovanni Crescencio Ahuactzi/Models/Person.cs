2b4b765bb52b490bf247fd3978cb908c028799b5
gyoandjoe <win-g@hotmail.com>
2016-01-30 21:17:59 -0600

    [Adicion de ejemplo de form modal en grid]

---
+++ Evaluación Giovanni Crescencio Ahuactzi/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Evaluación_Giovanni_Crescencio_Ahuactzi.Models
+{
+    public class Person
+    {
+        public int IdPerson { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+    }
+}
