54224ebb05bdf1712d24a41757605e926cd191cf
Gyo <win-g@hotmail.com>
2016-04-27 22:54:13 -0500

    [Adicion de ejemplo con angular]

---
+++ CheckingASPNET/Controllers/AngularTestController.cs
@@ -0,0 +1,27 @@
+﻿using CheckingASPNET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CheckingASPNET.Controllers
+{
+    public class AngularTestController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ObtenerTodasLasPersonas()
+        {
+
+            List<Person> personas = new List<Person>();
+            personas.Add(new Person() { Apellido = "Sanchez", Email="Sanchez@hotmail.com", IdPerson=1, Nombre="Santiago" });
+            personas.Add(new Person() { Apellido = "Lopez", Email = "Lopez@hotmail.com", IdPerson = 1, Nombre = "Areli" });
+            personas.Add(new Person() { Apellido = "Vasquez", Email = "Vasquez@hotmail.com", IdPerson = 1, Nombre = "Batman" });
+
+            return Json(personas, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
